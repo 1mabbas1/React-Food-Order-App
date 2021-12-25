695237c3678ef8ded54054341a6aaf1bfd33785d
Mohammad Abbas <10mabbas@googlemail.com>
2021-12-25 15:54:34 +0000

    Added connection to a Firebase backend database + added loading and error screens

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,61 @@
 import styles from "./AvailableMeals.module.css";
-import DUMMY_MEALS from "./DummyMeals.js";
 import Card from "../UI/Card";
 import MealItem from "./MealItem";
+import { useEffect, useState } from "react";
 
 function AvailableMeals() {
-  const mealslist = DUMMY_MEALS.map((meal) => (
+  const [meals, setmeals] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  const [Error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://big-kahuna-brgr-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setmeals(loadedMeals);
+      setisLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setisLoading(false);
+      setError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.Loading}>
+        <h3>Loading...</h3>
+      </section>
+    );
+  }
+
+  if (Error) {
+    return (
+      <section className={styles.Loading}>
+        <h3>{Error}</h3>
+      </section>
+    );
+  }
+
+  const mealslist = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
